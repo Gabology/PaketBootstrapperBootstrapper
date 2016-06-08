e91aa47de530ce1cd05f9548ad81fb51cd981404
Gabriel A.S <gabriel.a.svensk@gmail.com>
2016-06-08 20:25:05 +0200

    Uploaded the bootstrapper script

---
+++ PaketBB.ps1
@@ -0,0 +1,14 @@
+﻿# Create .paket folder and navigate to it
+mkdir .paket
+cd .paket
+
+# This link should not die, hopefully.
+$paketBootstraperURL = "https://github.com/fsprojects/Paket/releases/download/2.66.9/paket.bootstrapper.exe"
+
+
+Write-Host "Downloading Paket Bootstraper..." -ForegroundColor Magenta
+Invoke-WebRequest -Uri $paketBootstraperURL -Method Get -OutFile "./paket.bootstrapper.exe"
+
+Write-Host "Successfully downloaded Paket Bootstrapper, running..." -ForegroundColor Green
+
+& "./paket.bootstrapper.exe"
